010d694b01c440c63a7cf37c6d7d4a33b537c0a8
Justin Delano <25873234+justin-delano@users.noreply.github.com>
2026-08-30 09:20:51 -0400

    feat(categories): list the category filter menu by leaf, path on hover

--- source/Views/ManageAchievements/ManageAchievementsCategoryTab.xaml.cs
+++ source/Views/ManageAchievements/ManageAchievementsCategoryTab.xaml.cs
@@ -126,6 +126,9 @@ namespace PlayniteAchievements.Views.ManageAchievements
                 ViewModel.CategoryLabelFilterOptions,
                 option => ViewModel.IsCategoryLabelFilterSelected(option),
                 (option, isSelected) => ViewModel.SetCategoryLabelFilterSelected(option, isSelected),
+                // Leaf in the item, full path on hover: a flat menu of paths sharing long prefixes
+                // is hard to scan, but two leaves can share a name under different parents.
+                AchievementCategoryTypeHelper.ToCategoryLeafDisplayText,
                 AchievementCategoryTypeHelper.ToCategoryLabelDisplayText);
         }
 
@@ -1040,7 +1043,8 @@ namespace PlayniteAchievements.Views.ManageAchievements
             IEnumerable<string> options,
             Func<string, bool> isSelected,
             Action<string, bool> setSelection,
-            Func<string, string> displayText = null)
+            Func<string, string> displayText = null,
+            Func<string, string> toolTipText = null)
         {
             if (button == null || menu == null || isSelected == null || setSelection == null)
             {
@@ -1059,6 +1063,7 @@ namespace PlayniteAchievements.Views.ManageAchievements
                 var item = new MenuItem
                 {
                     Header = displayText?.Invoke(option) ?? option,
+                    ToolTip = toolTipText?.Invoke(option),
                     IsCheckable = true,
                     StaysOpenOnClick = true,
                     IsChecked = isSelected(option)
